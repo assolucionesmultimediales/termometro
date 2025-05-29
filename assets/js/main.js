006aa817336ddc9dc2b2a1edb6eabc605e5c3e55
assolucionesmultimediales <multimediales.solutions@gmail.com>
2025-05-29 17:06:12 -0300

    ajustando

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ let statusDiv = document.getElementById("status");
 // coordenadas de la facultad
 const FACULTAD_LAT = -34.611334;
 const FACULTAD_LON = -58.436502;
-const RADIO_PERMITIDO = 550; 
+const RADIO_PERMITIDO = 450; 
 
 
 // variable global para el gráfico
@@ -86,6 +86,10 @@ async function guardarReporte() {
   navigator.geolocation.getCurrentPosition(async position => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
+
+    console.log(lat);
+    console.log(lon);
+
     const distancia = distanciaEnMetros(lat, lon, FACULTAD_LAT, FACULTAD_LON);
 
     if (distancia > RADIO_PERMITIDO) {
